405ea22bf35291b0bbaa5d9eaec7489ea9015b08
Alen Komic <akomic@gmail.com>
2017-10-04 15:58:01 +0200

    Adds Token auth capability.

--- consul2zabbix/consul2zabbix.py
+++ consul2zabbix/consul2zabbix.py
@@ -6,31 +6,46 @@ import requests
 import socket
 import md5
 
+# Set your token here
+token = None
+
+if len(sys.argv) == 1:
+    print("Usage: {} <discovery|status> [serviceID]".format(sys.argv[0]))
+    sys.exit(1)
+
+headers = {'X-Consul-Token': token} if token else {}
 
 nodeName = socket.gethostname()
-#print nodeName
 
 url = 'http://127.0.0.1:8500/v1/health/node/{0}'.format(nodeName)
-#print url
 
 
 def getDiscovery():
     discovery_list = {}
     discovery_list['data'] = []
 
-    nodeServices = requests.get(url).text
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        print r.status_code, r.text
+        sys.exit(1)
+
+    nodeServices = r.text
 
     services = json.loads(nodeServices)
     for service in services:
         if service['CheckID'] != 'serfHealth':
-            #print service['Status']
-            #print service['ServiceName']
             zbx_item = {"{#SERVICEID}": service['ServiceID']}
             discovery_list['data'].append(zbx_item)
     print json.dumps(discovery_list, indent=4, sort_keys=True)
 
 def getStatus(ServiceID):
-    nodeServices = requests.get(url).text
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        print r.status_code, r.text
+        sys.exit(1)
+
+    nodeServices = r.text
+
     services = json.loads(nodeServices)
     status = 0
     for service in services:
@@ -47,4 +62,3 @@ if action == 'discovery':
 elif action == 'status':
     serviceID = sys.argv[2]
     getStatus(serviceID)
-
