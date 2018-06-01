473804218ca5cf162195fc70577f55097bdac11e
Dmitry Muzyka <a5b1ca@gmail.com>
2018-06-01 13:52:16 +0300

    Update docker-stats.sh

--- docker/docker-stats.sh
+++ docker/docker-stats.sh
@@ -4,7 +4,11 @@
 
 function memory_stat() {
     container=$1
-    pids=`cat /sys/fs/cgroup/cpu/docker/${container}/cgroup.procs`
+    if [ -d "/sys/fs/cgroup/cpu/docker" ]; then
+        pids=`cat "/sys/fs/cgroup/cpu/docker/${container}/cgroup.procs"`
+    else
+        pids=`cat "/sys/fs/cgroup/cpu/system.slice/docker-${container}.scope/cgroup.procs"`
+    fi
     mem=0
 #    pids=`echo $pids | tr " " "|"`
 #    ps aux | awk "\$2 ~ \"^($pids)\$\" {SUM += \$6} END {print SUM}"
@@ -16,13 +20,21 @@ function memory_stat() {
 
 function process_count() {
     container=$1
-    cat /sys/fs/cgroup/cpu/docker/${container}/cgroup.procs | wc -l
+    if [ -d "/sys/fs/cgroup/cpu/docker" ]; then
+        cat /sys/fs/cgroup/cpu/docker/${container}/cgroup.procs | wc -l
+    else
+        cat /sys/fs/cgroup/cpu/system.slice/docker-${container}.scope/cgroup.procs | wc -l
+    fi
 }
 
 function cpu_usage() {
     type=$1
     container=$2
-    cat /sys/fs/cgroup/cpuacct/docker/${container}/cpuacct.stat |  grep ${type} | awk '{print $2}'
+    if [ -d "/sys/fs/cgroup/cpuacct/docker" ]; then
+        cat /sys/fs/cgroup/cpuacct/docker/${container}/cpuacct.stat |  grep ${type} | awk '{print $2}'
+    else
+        cat /sys/fs/cgroup/cpuacct/system.slice/docker-${container}.scope/cpuacct.stat |  grep ${type} | awk '{print $2}'
+    fi
 }
 
 case $1 in
